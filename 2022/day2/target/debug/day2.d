f8880fa82f2c4c6a4b7c6b9057f5d4b42d982393
Regalk <jsalazarcaviedes30@gmail.com>
2022-12-03 09:52:19 -0500

    Add day2

--- 2022/day2/target/debug/day2.d
+++
@@ -1,1 +0,0 @@
-/home/regalk/Projects/AdventOfCode/2022/day2/target/debug/day2: /home/regalk/Projects/AdventOfCode/2022/day2/src/main.rs
--- 2022/day2/target/debug/deps/day2-a371407917746094.d
+++
@@ -1,5 +0,0 @@
-/home/regalk/Projects/AdventOfCode/2022/day2/target/debug/deps/day2-a371407917746094: src/main.rs
-
-/home/regalk/Projects/AdventOfCode/2022/day2/target/debug/deps/day2-a371407917746094.d: src/main.rs
-
-src/main.rs:
