879b1950770ed863510588ec1303789173b25d54
Regalk <jsalazarcaviedes30@gmail.com>
2022-12-14 10:23:38 -0500

    Add solution day14 part1

---
+++ 2022/day14/src/main.rs
@@ -0,0 +1,122 @@
+use std::collections::HashMap;
+
+#[derive(Default, Debug)]
+struct Cave {
+    location: HashMap<(i32, i32), char>,
+    bottom: i32,
+}
+
+impl Cave {
+    fn convert(s: Option<&str>) -> Option<(i32, i32)> {
+        if let Some(s) = s {
+            let (x, y) = s.split_once(",").unwrap();
+            Some((x.parse().unwrap(), y.parse().unwrap()))
+        } else {
+            None
+        }
+    }
+
+    fn draw_line(&mut self, start: (i32, i32), end: (i32, i32)) {
+        let dx = (end.0 - start.0).signum();
+        let dy = (end.1 - start.1).signum();
+        
+        self.bottom = self.bottom.max(start.1.max(end.1));
+
+        let mut point = start;
+        self.location.insert(point, '#');
+        while point != end {
+            point.0 += dx;
+            point.1 += dy;
+            self.location.insert(point, '#');
+        }       
+    }
+    
+    fn drop_sand(&mut self) -> bool {
+        let mut sand: (i32, i32) = (500, 0);
+        
+        while let Some(next_pos) = self.fall(sand) {
+            if next_pos.1 > self.bottom {
+                return false;
+            }
+            sand = next_pos;
+        }
+
+        self.location.insert(sand, 'o');
+        true
+    }
+
+    fn fall(&self, pos: (i32, i32)) -> Option<(i32,i32)> {
+        for delta_x in [0, -1, 1] {
+            let new_pos = (pos.0 + delta_x, pos.1 + 1);
+            if !self.location.contains_key(&new_pos) {
+                return Some(new_pos);
+            }
+        }
+        None
+    }
+
+    fn display(&self) {
+        for y in 0..=9 {
+            for x in 495..=503 {
+                print!("{}", 
+                       if x == 500 && y == 0 { '+' }
+                       else if let Some(ch) = self.location.get(&(x,y)){ *ch } else { '.' });
+            }
+            println!("");
+        }
+    }
+
+}
+
+fn first_part(file: &str) {
+    let mut cave = Cave::default();
+    
+    for line in file.trim().split("\n").collect::<Vec<&str>>() {
+        let mut iter = line.split(" -> ");
+        let mut start = Cave::convert(iter.next()).unwrap();
+        while let Some(end) = Cave::convert(iter.next()) {
+            cave.draw_line(start, end);
+            start = end;
+        }
+    }
+   
+    cave.display();
+    let mut output = 0;
+    while cave.drop_sand() {
+        output += 1;
+    }
+    
+    cave.display();
+
+    println!("Output: {}", output);
+   
+}
+
+fn second_part(file: &str) {
+    let mut cave = Cave::default();
+    
+    for line in file.trim().split("\n").collect::<Vec<&str>>() {
+        let mut iter = line.split(" -> ");
+        let mut start = Cave::convert(iter.next()).unwrap();
+        while let Some(end) = Cave::convert(iter.next()) {
+            cave.draw_line(start, end);
+            start = end;
+        }
+    }
+   
+    cave.display();
+    let mut output = 0;
+    while cave.drop_sand() {
+        output += 1;
+    }
+    
+    cave.display();
+
+    println!("Output: {}", output);
+ 
+}
+fn main() {
+    let file = std::fs::read_to_string("./input").expect("Couldn't read input file");
+    // first_part(&file);  
+    second_part(&file); 
+}
