5f96c0eb0b6357a832aa4dfb8e1e2e75afc4014f
Regalk <jsalazarcaviedes30@gmail.com>
2022-12-18 09:28:46 -0500

    Add solution day18 part2

--- 2022/day18/src/main.rs
+++ 2022/day18/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet, VecDeque};
 use ordered_float::OrderedFloat;
 
 fn first_part(file: &str) -> i32 {
@@ -27,12 +27,25 @@ fn first_part(file: &str) -> i32 {
 
 fn second_part(file: &str) -> i32 {
     let mut faces: HashMap<(OrderedFloat<f32>, OrderedFloat<f32>, OrderedFloat<f32>), i32> = HashMap::new();
+    let mut droplet = HashSet::new();
     let offsets = vec![(0.0, 0.0, 0.5), (0.0, 0.5, 0.0), (0.5, 0.0, 0.0), (0.0, 0.0, -0.5), (0.0, -0.5, 0.0), (-0.5, 0.0, 0.0)];
     
+    let (mut mx, mut my, mut mz) = (f32::INFINITY, f32::INFINITY, f32::INFINITY);
+    let (mut max, mut maxy, mut maxz) = (f32::NEG_INFINITY, f32::NEG_INFINITY, f32::NEG_INFINITY);
+
+
     for line in file.trim().split("\n").collect::<Vec<&str>>() {
         let positions  = line.split(",").map(|l| l.parse::<u32>().unwrap()).collect::<Vec<u32>>();
-        let (x, y, z) = (positions[0], positions[1], positions[2]);
-        
+        let (x, y, z) = (positions[0] as f32, positions[1] as f32, positions[2] as f32);
+        droplet.insert((OrderedFloat(x), OrderedFloat(y), OrderedFloat(z)));
+        mx = mx.min(x);
+        my = my.min(y);
+        mz = mz.min(z);
+    
+        max = max.max(x);
+        maxy = maxy.max(y);
+        maxz = maxz.max(z);
+    
         for (dx, dy, dz) in &offsets {
             let k = (OrderedFloat(x as f32 + dx), OrderedFloat(y as f32 + dy), OrderedFloat(dz + z as f32));
             
@@ -44,9 +57,57 @@ fn second_part(file: &str) -> i32 {
             }
         }
     }
-    let output: i32 = faces.iter().map(|(_, v)| if *v == 1 { return *v; } else { return 0 as i32; }).sum();
-    output
 
+    mx -= 1.0;
+    my -= 1.0;
+    mz -= 1.0;
+
+    max += 1.0;
+    maxy += 1.0;
+    maxz += 1.0;
+     
+    let mut q = VecDeque::from([(mx, my, mz)]);
+
+    let mut air = HashSet::new();
+    air.insert((OrderedFloat(mx), OrderedFloat(my), OrderedFloat(mz)));
+    
+    while !(q.is_empty()) {
+
+        let (x, y, z) = q.pop_front().unwrap();
+        for (dx, dy, dz) in &offsets {
+            let key = (OrderedFloat(x + dx * 2.0), OrderedFloat(y + dy * 2.0), OrderedFloat(z + dz * 2.0));
+            let k = (x + dx * 2.0, y + dy * 2.0, z + dz * 2.0);
+            let (nx, ny, nz) = key;
+
+            if !(mx <= *nx && nx <= OrderedFloat(max) && my <= *ny && ny <= OrderedFloat(maxy) && mz <= *nz && nz <= OrderedFloat(maxz)) {
+                continue;       
+            }
+
+            if droplet.contains(&key) || air.contains(&key) {
+                continue;
+            }
+
+            air.insert(key);
+            q.push_back(k);
+        }
+    }
+
+    let mut free = HashSet::new();
+
+    for (x, y, z) in air {
+        for (dx, dy, dz) in &offsets {
+             free.insert((x + OrderedFloat(*dx), y + OrderedFloat(*dy), OrderedFloat(*dz) + z));
+        }
+    }
+
+    let mut total = 0;
+
+    for (key, _) in &faces {
+        if free.contains(&key) {
+            total += 1;
+        }
+    }
+    total    
 }
 
 fn main() {
