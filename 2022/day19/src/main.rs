661c4e516fba113b1ffcc63bfeef3d82301a9314
Regalk <jsalazarcaviedes30@gmail.com>
2022-12-19 14:57:09 -0500

    Add solution day19 part1-part2

--- 2022/day19/src/main.rs
+++ 2022/day19/src/main.rs
@@ -1,74 +1,219 @@
-use regex::Regex;
-use std::collections::HashMap;
+use std::collections::HashSet;
+use itertools::Itertools;
 
-fn dfs(bp: Vec<Vec<(i32, i32)>>, max_spend: Vec<i32>, mut cache: HashMap<(i32, i32, i32, i32, i32, i32, i32, i32, i32), i32>, time: i32, bots: Vec<i32>, amt: Vec<i32>)  -> i32{
-    if time == 0 {
-	    return amt[3]
-    }
-
-    let key = (time, bots[0], bots[1], bots[2], bots[3], amt[0], amt[1], amt[2], amt[3]);
-
-    if cache.contains_key(&key) {
-	    return cache[&key];
-    }
-
-    let mut max_val = amt[3] + bots[3] * time;
-
-    for (btype, recipe) in bp.iter().enumerate() {
-	if btype != 3  && bots[btype] >= max_spend[btype] {
-	    continue;
-	}
-	let mut wait = 0;
-
-	for (ramt, rtype) in recipe {
-	    if bots[*rtype as usize] == 0{
-		    break;
-	    }
-	    wait = wait.max(-(-(*ramt - amt[*rtype as usize]) / bots[*rtype as usize]));
-	}
-	let remtime = time - wait - 1;
-	if remtime <= 0 {
-	    continue
-	}
-	let mut bots_ = bots.clone();
-	let mut amt_ = std::iter::zip(amt.clone(), bots.clone()).map(|(x, y)|x + y * (wait + 1)).collect::<Vec<i32>>();
-    println!("amt_: {:?}", amt_);
-	for (ramt, rtype) in recipe {
-	    amt_[*rtype as usize] -= ramt;
-	}
-	bots_[btype] += 1;
-	for i in 0..3 {
-	    amt_[i] = amt_[i].min(max_spend[i] * remtime);
-	}
-	    max_val = max_val.max(dfs(bp.clone(), max_spend.clone(), cache.clone(), remtime, bots_, amt_));
-    }
-    cache.insert(key, max_val);
-    return max_val;
+#[derive(Debug, Default, PartialEq, Eq, Clone, Copy, Hash)]
+struct State {
+    time_left: i32,
+    ore: i32,
+    clay: i32,
+    obs: i32,
+    geode: i32,
+    ore_robot: i32,
+    clay_robot: i32,
+    obs_robot: i32,
+    geode_robot: i32,
+}
+#[derive(Debug, PartialEq, Eq)]
+struct Blueprint {
+    idx: i32,
+    ore_cost_in_ore: i32,
+    clay_cost_in_ore: i32,
+    obs_cost_in_ore: i32,
+    obs_cost_in_clay: i32,
+    geode_cost_in_ore: i32,
+    geode_cost_in_obsidian: i32,
 }
 
+pub fn first_part(input: &str) ->i32 {
+    let blueprints = parse(input);
+    blueprints
+        .iter()
+        .map(|b| {
+            b.idx
+                * get_best(
+                    &b,
+                    State {
+                        time_left: 24,
+                        ore: 0,
+                        clay: 0,
+                        obs: 0,
+                        geode: 0,
+                        ore_robot: 1,
+                        clay_robot: 0,
+                        obs_robot: 0,
+                        geode_robot: 0,
+                    },
+                )
+        })
+        .sum()
+}
+pub fn second_part(input: &str) -> i32 {
+    let blueprints = parse(input);
+    blueprints
+        .iter()
+        .take(3)
+        .map(|b| {
+            get_best(
+                &b,
+                State {
+                    time_left: 32,
+                    ore: 0,
+                    clay: 0,
+                    obs: 0,
+                    geode: 0,
+                    ore_robot: 1,
+                    clay_robot: 0,
+                    obs_robot: 0,
+                    geode_robot: 0,
+                },
+            )
+        })
+        .product()
+}
+
+fn get_best(blueprint: &Blueprint, start_state: State) -> i32 {
+    let mut scanned: HashSet<State> = HashSet::with_capacity(50 * 1024 * 1024);
+    let mut to_scan = Vec::with_capacity(50 * 1024 * 1024);
+    to_scan.push(start_state);
+
+    // we use the current best state in terms of geodes
+    // to discover if the evaluated state that will always be worse than the current best
+    // if it is, we stop there.
+    let mut best_geode = 0;
+    let mut best_state = State::default();
+
+    // as we can only build one robot a turn,
+    // there is no point in producing more than what we can spend
+    let max_ore = *[
+        blueprint.clay_cost_in_ore,
+        blueprint.ore_cost_in_ore,
+        blueprint.obs_cost_in_ore,
+        blueprint.geode_cost_in_ore,
+    ]
+    .iter()
+    .max()
+    .unwrap();
+    let max_obsidian = blueprint.geode_cost_in_obsidian;
+    let max_clay = blueprint.obs_cost_in_clay;
+
+    while let Some(state) = to_scan.pop() {
+        let geode_expected = state.geode + state.time_left * state.geode_robot;
+        if state.time_left == 1 {
+            if best_geode < geode_expected {
+                best_geode = geode_expected;
+                best_state = state;
+            }
+            continue; // end of investigation
+        }
+
+        if worse(best_state, state) {
+            continue; // will always be worse than the best
+        }
+
+        if scanned.contains(&state) {
+            continue; // already processed.
+        }
+        scanned.insert(state);
+
+        if best_geode < geode_expected {
+            best_geode = geode_expected;
+            best_state = state;
+        }
+
+        to_scan.push(dig(state));
+        // geode robot
+        if state.ore >= blueprint.geode_cost_in_ore && state.obs >= blueprint.geode_cost_in_obsidian
+        {
+            let mut new_state = dig(state);
+            new_state.geode_robot += 1;
+            new_state.obs -= blueprint.geode_cost_in_obsidian;
+            new_state.ore -= blueprint.geode_cost_in_ore;
+            to_scan.push(new_state);
+            continue; // seems to always be better to build a geode robot when we
+        }
+        // obsidian robot
+        if state.ore >= blueprint.obs_cost_in_ore
+            && state.clay >= blueprint.obs_cost_in_clay
+            && !dont_need_more(state.time_left, state.obs, state.obs_robot, max_obsidian)
+        {
+            let mut new_state = dig(state);
+            new_state.obs_robot += 1;
+            new_state.clay -= blueprint.obs_cost_in_clay;
+            new_state.ore -= blueprint.obs_cost_in_ore;
+            to_scan.push(new_state);
+        }
+        // clay robot payoffs in >3turns
+        if state.ore >= blueprint.clay_cost_in_ore
+            && !dont_need_more(state.time_left, state.clay, state.clay_robot, max_clay)
+            && state.time_left > 3
+        {
+            let mut new_state = dig(state);
+            new_state.clay_robot += 1;
+            new_state.ore -= blueprint.clay_cost_in_ore;
+            to_scan.push(new_state);
+        }
+        // ore robot
+        if state.ore >= blueprint.ore_cost_in_ore
+            && !dont_need_more(state.time_left, state.ore, state.ore_robot, max_ore)
+        {
+            let mut new_state = dig(state);
+            new_state.ore_robot += 1;
+            new_state.ore -= blueprint.ore_cost_in_ore;
+            to_scan.push(new_state);
+        }
+    }
+    best_geode
+}
+
+fn dont_need_more(time_left: i32, stock: i32, robots: i32, max: i32) -> bool {
+    robots >= max || time_left * robots + stock > time_left * max
+}
+
+fn worse(max: State, other: State) -> bool {
+    (max.time_left == other.time_left
+        && max.geode_robot >= other.geode_robot
+        && max.obs_robot >= other.obs_robot
+        && max.clay_robot >= other.clay_robot
+        && max.ore_robot >= other.ore_robot
+        && max.geode >= other.geode
+        && max.obs >= other.obs
+        && max.clay >= other.clay
+        && max.ore >= other.ore)
+        || other.geode
+            + other.geode_robot * other.time_left
+            + other.time_left * (other.time_left / 2)
+            <= max.geode + max.geode_robot * max.time_left
+}
+
+fn dig(mut state: State) -> State {
+    state.ore += state.ore_robot;
+    state.clay += state.clay_robot;
+    state.obs += state.obs_robot;
+    state.geode += state.geode_robot;
+    state.time_left -= 1;
+    state
+}
+
+fn parse(input: &str) -> Vec<Blueprint> {
+    input
+        .lines()
+        .map(|l| {
+            let el = l.split_ascii_whitespace().collect_vec();
+            Blueprint {
+                idx: el[1][..el[1].len() - 1].parse::<i32>().unwrap(),
+                ore_cost_in_ore: el[6].parse::<i32>().unwrap(),
+                clay_cost_in_ore: el[12].parse::<i32>().unwrap(),
+                obs_cost_in_ore: el[18].parse::<i32>().unwrap(),
+                obs_cost_in_clay: el[21].parse::<i32>().unwrap(),
+                geode_cost_in_ore: el[27].parse::<i32>().unwrap(),
+                geode_cost_in_obsidian: el[30].parse::<i32>().unwrap(),
+            }
+        })
+        .collect_vec()
+}
 
 fn main() {
-    let mut total = 0;
-    let file = std::fs::read_to_string("./input.test").expect("Expected file");
-    for (i, l) in file.trim().split("\n").collect::<Vec<&str>>().iter().enumerate() {
-	let mut bp = vec![];
-	let mut max_spend: Vec<i32> = vec![0, 0, 0];
-	for section in l.split(": ").collect::<Vec<&str>>()[1].split(". ").collect::<Vec<&str>>() {
-	    let regex = Regex::new(r"(\d+) (\w+)").unwrap();
-	    let mut recipe = vec![];
-	    for r in regex.captures_iter(section) {
-		let robots = vec!["ore", "clay", "obsidian"];
-		let x = r[1].parse::<i32>().unwrap();
-		let y = robots.iter().position(|&x| *x == r[2]).unwrap();
-		recipe.push((x,y as i32));
-		max_spend[y] = x.max(max_spend[y]); 
-	    }
-	    bp.push(recipe);
-	}
-
-	let v = dfs(bp, max_spend, HashMap::new(), 24, [1, 0, 0, 0].to_vec(), [0, 0, 0, 0].to_vec());
-	total += (i as i32 + 1) * v;
-    }
-
-    println!("Total: {}", total);
+    let file = std::fs::read_to_string("./input").expect("Expected file");
+    println!("Output 1: {:?}", first_part(&file));
+    println!("Output 2: {:?}", second_part(&file));
 }
