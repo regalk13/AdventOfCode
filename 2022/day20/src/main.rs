ea4cb7c4d5aca7cb46f9c9d201ad59455f7299e3
Regalk <jsalazarcaviedes30@gmail.com>
2022-12-20 12:48:29 -0500

    Add comments

--- 2022/day20/src/main.rs
+++ 2022/day20/src/main.rs
@@ -1,3 +1,4 @@
+// Parsing the input, this returns a vector of tuples (0 <- index, 1 <- value)
 fn parse(input: &str) -> Vec<(i32, i32)> {
     input
         .trim()
@@ -12,31 +13,42 @@ fn parse(input: &str) -> Vec<(i32, i32)> {
 
 fn first_part(file: &str) -> i32 {
     let parsed = parse(file);
+    // Clone of the parsed input, mutable for changes
     let mut output = parsed.clone();
+    
     for (i, _) in parsed.iter() {
+        // Position of the current index in the loop
         let position = output.iter().position(|&x| x.0 == *i as i32).unwrap();
-
+        // Get and remove the value
         let current = output.remove(position);
+        // Index + value
         let add = position as i64 + current.1 as i64;
-
+        
+        // Get the value euclid include negative positions! 
         let new_pos = add.rem_euclid(output.len() as i64);
         // println!("Current position: {:?} pos: {}", current, new_pos);
         output.insert(new_pos as usize, current);
     }
+    // Position of zero
     let zero = output.iter().position(|v| v.1 == 0).unwrap();
+    // 1000, 2000 and 3000 cycles from 0 
     let a = output[(1000 + zero) % output.len()].1;
     let b = output[(2000 + zero) % output.len()].1;
     let c = output[(3000 + zero) % output.len()].1;
+    // return the sum!
     a + b + c as i32
 }
 
 fn second_part(file: &str) -> i64 {
+    // Decrypt key
     let d_key = 811589153;
+    // Multiply each time by d_key and convert it to i64
     let parsed = parse(file)
         .iter()
         .map(|(i, s)| (*i as i64, *s as i64 * d_key as i64))
         .collect::<Vec<(i64, i64)>>();
     let mut output = parsed.clone();
+   // move items 10 times
     for _ in 0..10 {
         for (i, _) in parsed.iter() {
             let position = output.iter().position(|&x| x.0 == *i as i64).unwrap();
@@ -49,7 +61,9 @@ fn second_part(file: &str) -> i64 {
             output.insert(new_pos as usize, current);
         }
     }
+    // zero position
     let zero = output.iter().position(|v| v.1 == 0).unwrap();
+    // 1000, 2000 and 3000 cycles from 0 
     let a = output[(1000 + zero) % output.len()].1;
     let b = output[(2000 + zero) % output.len()].1;
     let c = output[(3000 + zero) % output.len()].1;
