08661bb2e693c9b47abc5f00dab1e6196939fb5a
Regalk <jsalazarcaviedes30@gmail.com>
2022-12-21 13:09:49 -0500

    Formating...

--- 2022/day21/src/main.rs
+++ 2022/day21/src/main.rs
@@ -5,7 +5,7 @@ enum Operation {
     Add,
     Sub,
     Mult,
-    Div
+    Div,
 }
 
 impl Operation {
@@ -14,7 +14,7 @@ impl Operation {
             '+' => Self::Add,
             '-' => Self::Sub,
             '*' => Self::Mult,
-            '/'=> Self::Div,
+            '/' => Self::Div,
             _ => panic!("Invalid operation: {}", ch),
         }
     }
@@ -32,22 +32,26 @@ fn parser(file: &str) -> HashMap<String, Value> {
         .lines()
         .map(|line| {
             let (name, value) = line.split_once(": ").unwrap();
-            
-            if let Ok(num) = value.parse::<i64>()
-            {
-                (name.to_string(), Value::Integer(num))
-            } else 
-            {
-                let expr = value.split_whitespace().collect::<Vec<&str>>();
-                (name.to_string(), Value::Op(expr[0].to_string(), Operation::parse(expr[1].chars().next().unwrap()), expr[2].to_string()))
-            }
 
+            if let Ok(num) = value.parse::<i64>() {
+                (name.to_string(), Value::Integer(num))
+            } else {
+                let expr = value.split_whitespace().collect::<Vec<&str>>();
+                (
+                    name.to_string(),
+                    Value::Op(
+                        expr[0].to_string(),
+                        Operation::parse(expr[1].chars().next().unwrap()),
+                        expr[2].to_string(),
+                    ),
+                )
+            }
         })
         .collect::<HashMap<String, Value>>()
 }
 
 fn make_operation(hash: &HashMap<String, Value>, key: &String) -> i64 {
-   let value = hash.get(key).unwrap();
+    let value = hash.get(key).unwrap();
     match value {
         Value::Integer(v) => *v,
         Value::Op(l, op, r) => {
@@ -67,7 +71,7 @@ fn find_me<'a>(loc: &'a String, tree: &'a HashMap<String, Value>) -> Option<Vec<
     if loc == "humn" {
         return Some(vec![loc]);
     }
-    if let Some(Value::Op(l, _, r)) =  tree.get(loc) {
+    if let Some(Value::Op(l, _, r)) = tree.get(loc) {
         if let Some(mut vec) = find_me(l, tree) {
             vec.push(loc);
             return Some(vec);
@@ -81,13 +85,18 @@ fn find_me<'a>(loc: &'a String, tree: &'a HashMap<String, Value>) -> Option<Vec<
     None
 }
 
-fn find_adjustment(path: &Vec<&String>, index: usize, tree: &HashMap<String, Value>, cv: i64) -> i64 {
+fn find_adjustment(
+    path: &Vec<&String>,
+    index: usize,
+    tree: &HashMap<String, Value>,
+    cv: i64,
+) -> i64 {
     match tree.get(path[index]).unwrap() {
         Value::Integer(_) => cv,
         Value::Op(l, op, r) => {
             let left = make_operation(tree, l);
             let right = make_operation(tree, r);
-            let new_cv = if l == path[index +  1] {
+            let new_cv = if l == path[index + 1] {
                 match op {
                     Operation::Add => cv - right,
                     Operation::Sub => cv + right,
@@ -111,7 +120,7 @@ fn second_part(hash: &HashMap<String, Value>) -> i64 {
     let root_name = "root".to_string();
     let path = find_me(&root_name, &hash).unwrap();
     let path = path.iter().rev().copied().collect::<Vec<_>>();
-    
+
     let (left, right) = match hash.get(&root_name).unwrap() {
         Value::Integer(_) => panic!("Root without data"),
         Value::Op(left, _, right) => (left, right),
@@ -126,12 +135,10 @@ fn second_part(hash: &HashMap<String, Value>) -> i64 {
     find_adjustment(&path, 1, &hash, right_num)
 }
 
-
 fn first_part(hash: &HashMap<String, Value>) -> i64 {
     make_operation(hash, &"root".to_string())
-
 }
-fn main() {    
+fn main() {
     let file = std::fs::read_to_string("./input").expect("Couldn't read input file");
     let parsed = parser(&file);
     println!("Output 1: {:?}", first_part(&parsed));
