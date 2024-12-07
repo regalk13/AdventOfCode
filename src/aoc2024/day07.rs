77e43d40c9b3039f74da81aaca64caab4294f0fa
regalk <72028266+regalk13@users.noreply.github.com>
2024-12-07 10:07:39 -0500

    feat: day07

---
+++ src/aoc2024/day07.rs
@@ -0,0 +1,99 @@
+use crate::Runit;
+
+#[derive(Default)]
+pub struct AocDay07 {
+    lines: Vec<Calibration>,
+}
+
+#[derive(Clone)]
+struct Calibration {
+    target: i64,
+    values: Vec<i64>,
+}
+
+impl AocDay07 {
+    pub fn new() -> Self {
+        Self::default()
+    }
+}
+
+impl AocDay07 {
+    fn obtain(&mut self, target: i64, values: &[i64], second_p: bool) -> bool {
+        let n = values.len();
+
+        if n == 1 {
+            return target == values[0];
+        }
+
+        if target % values[n - 1] == 0
+            && self.obtain(target / values[n - 1], &values[..n - 1], second_p)
+        {
+            return true;
+        }
+
+        if target > values[n - 1]
+            && self.obtain(target - values[n - 1], &values[..n - 1], second_p)
+        {
+            return true;
+        }
+
+        if second_p {
+            let s_target = target.to_string();
+            let s_last = values[n - 1].to_string();
+    
+            if s_target.len() > s_last.len()
+                && s_target.ends_with(&s_last)
+            {
+                let truncated = &s_target[..s_target.len() - s_last.len()];
+                if let Ok(truncated_value) = truncated.parse::<i64>() {
+                    if self.obtain(truncated_value, &values[..n - 1], second_p) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        false
+    }
+}
+
+impl Runit for AocDay07 {
+    fn parse(&mut self) {
+        let file = crate::read_file("2024".to_string(), "07".to_string());
+
+        self.lines = file
+            .split("\n")
+            .filter(|s| !s.is_empty())
+            .map(|s| {
+                let line: Vec<_> = s.split(": ").collect();
+                let target = line[0].parse::<i64>().unwrap();
+                let values = line[1]
+                    .split(" ")
+                    .filter(|s| !s.is_empty())
+                    .map(|s| s.parse::<i64>().unwrap())
+                    .collect();
+                Calibration { target, values }
+            })
+            .collect();
+    }
+
+    fn first_part(&mut self) -> String {
+        self.lines
+            .clone()
+            .iter()
+            .filter(|calibration| self.obtain(calibration.target, &calibration.values, false))
+            .map(|calibration| calibration.target)
+            .sum::<i64>()
+            .to_string()
+    }
+
+    fn second_part(&mut self) -> String {
+        self.lines
+            .clone()
+            .into_iter()
+            .filter(|calibration| self.obtain(calibration.target, &calibration.values, true))
+            .map(|calibration| calibration.target)
+            .sum::<i64>()
+            .to_string() 
+    }
+}
--- src/aoc2024/mod.rs
+++ src/aoc2024/mod.rs
@@ -5,6 +5,7 @@ mod day03;
 mod day04;
 mod day05;
 mod day06;
+mod day07;
 
 pub fn run_2024(day: u32) {
     let mut day01 = day01::AocDay01::new();
@@ -13,8 +14,9 @@ pub fn run_2024(day: u32) {
     let mut day04 = day04::AocDay04::new();
     let mut day05 = day05::AocDay05::new();
     let mut day06 = day06::AocDay06::new();
+    let mut day07 = day07::AocDay07::new();
 
-    let mut days: Vec<&mut dyn Runit> = vec![&mut day01, &mut day02, &mut day03, &mut day04, &mut day05, &mut day06];
+    let mut days: Vec<&mut dyn Runit> = vec![&mut day01, &mut day02, &mut day03, &mut day04, &mut day05, &mut day06, &mut day07];
 
     if day == 0 || (day - 1) >= days.len() as u32 {
         println!("Invalid day! it's out of the range");
